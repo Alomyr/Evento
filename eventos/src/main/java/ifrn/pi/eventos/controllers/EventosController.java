0cb7010dfc9368753635b563454327aae857dd96
Alomyr <matheuscastro11213@gmail.com>
2021-02-13 16:49:41 -0300

    up4

--- eventos/src/main/java/ifrn/pi/eventos/controllers/EventosController.java
+++ eventos/src/main/java/ifrn/pi/eventos/controllers/EventosController.java
@@ -1,12 +1,21 @@
 package ifrn.pi.eventos.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import ifrn.pi.eventos.models.Evento;
 
 @Controller
 public class EventosController {
-@RequestMapping ("/cadastro")
+	@RequestMapping("/cadastro")
 	public String form() {
 		return "cadastro";
+	}
+
+	@PostMapping("/eventos")
+	public String adicionar(Evento evento) {
+		System.out.println(evento);
+		return "evento-adicionado";
 	}
 }
--- eventos/src/main/java/ifrn/pi/eventos/controllers/IndexController.java
+++ eventos/src/main/java/ifrn/pi/eventos/controllers/IndexController.java
@@ -7,7 +7,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class IndexController {
 @RequestMapping("/")
 	public String index() {
-	System.out.println("acessou ");
 		return"home";
 	}
 }
